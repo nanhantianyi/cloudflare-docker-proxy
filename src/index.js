3399804f0d2b277bef1cfba7ee61d757ea6aa6bf
kevin <nanhantianyi@126.com>
2024-06-15 19:57:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.catserver.top": dockerHub,
+  "quay.catserver.top": "https://quay.io",
+  "gcr.catserver.top": "https://gcr.io",
+  "k8s-gcr.catserver.top": "https://k8s.gcr.io",
+  "k8s.catserver.top": "https://registry.k8s.io",
+  "ghcr.catserver.top": "https://ghcr.io",
+  "cloudsmith.catserver.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.catserver.top": dockerHub,
 };
 
 function routeByHosts(host) {
